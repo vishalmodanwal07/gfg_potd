230cc76923a83880f0137c9a0ca2419d7a08acc4
vishal modanwal <vk0764394@gmail.com>
2023-05-14 00:18:43 +0530

    1st problem

--- BitMagic.java
+++ BitMagic.java
@@ -1,3 +1,4 @@
+https://practice.geeksforgeeks.org/problems/ed0422e992899f3f46340ce97b0090683ceebd67/1  
 class Solution {
     public static int bitMagic(int n, int[] arr) {
       int ans=0,i=0,j=n-1;
