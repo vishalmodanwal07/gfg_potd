a0fbdee24d87351ff821cab38599022cca17adbd
vishal modanwal <vk0764394@gmail.com>
2023-05-15 11:49:46 +0530

    2nd problem

---
+++ MaximumsubsetSum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int bitMagic(int n, int[] arr) {
+      int ans=0,i=0,j=n-1;
+        while(i<j){
+            if(arr[i]!=arr[j])ans++;
+            i++;
+            j-- ;
+        }
+        return ans/2 + ans%2;
+}
+}   
+https://practice.geeksforgeeks.org/problems/ed0422e992899f3f46340ce97b0090683ceebd67/1    
